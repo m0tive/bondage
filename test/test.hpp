c67a7a3c32a7834d8bd1848858918a04c406a2a0
George Pimm <george.pimm@naturalmotion.com>
2014-01-15 13:44:19 +0000

    initial commit

---
+++ test/test.hpp
@@ -0,0 +1,26 @@
+
+#define PORK
+/// \brief Test
+class PORK Foo
+{
+  /// \brief Test
+  class SubFoo // Test
+  {
+  /// \brief Test
+    void setX(float );
+    float x;
+  };
+  
+  /// \brief Test
+  void bar();
+  // test.
+  int cake()
+  {
+    printf("");
+  }
+};
+
+// Test
+class PORK Foo
+{
+};
