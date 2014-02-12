d552ef094c07a653569b97f4c334b2852cbf64e3
Jorj <jorjpimm@gmail.com>
2014-02-12 22:06:03 +0000

    churn

--- parser/ParserStateItem.rb
+++ parser/ParserStateItem.rb
@@ -46,27 +46,51 @@ private
     }
   end
 
+  # Extract a comment into [toFill] (a Comment), recursing into child comments
   def extractComment(toFill, comment)
-    if(comment.kind_of?(FFI::Clang::Comment))
-      if(comment.kind_of?(FFI::Clang::TextComment) || comment.kind_of?(FFI::Clang::ParagraphComment))
-        if(toFill.command("brief") == "")
-          toFill.addCommand("brief", comment.text)
-        end
-      elsif(comment.kind_of?(FFI::Clang::BlockCommandComment))
-        toFill.addCommand(comment.name, comment.comment)
-      elsif(comment.kind_of?(FFI::Clang::InlineCommandComment))
-        toFill.addCommand(comment.name, "")
-      elsif(comment.kind_of?(FFI::Clang::ParamCommandComment))
-        if(comment.valid_index?)
-          toFill.addParam(comment.index, comment.comment, comment.direction_explicit?, comment.direction)
-        end
-      end
+    raise "invalid comment passed #{comment}" unless comment.kind_of?(FFI::Clang::Comment)
 
-      comment.each do |comment|
-        extractComment(toFill, comment)
-      end
+    if(comment.kind_of?(FFI::Clang::TextComment) || comment.kind_of?(FFI::Clang::ParagraphComment))
+      extractTextComment(toFill, comment)
+
+    elsif(comment.kind_of?(FFI::Clang::BlockCommandComment))
+      extractBlockCommandComment(toFill, comment)
+
+    elsif(comment.kind_of?(FFI::Clang::InlineCommandComment))
+      extractInlineCommandComment(toFill, comment)
+
+    elsif(comment.kind_of?(FFI::Clang::ParamCommandComment))
+      extractParamCommandComment(toFill, comment)
+    end
+
+    comment.each do |comment|
+      extractComment(toFill, comment)
     end
 
     return comment
+  end
+
+  # Extract a text comment into [toFill]
+  def extractTextComment(toFill, comment)
+    if(toFill.command("brief") == "")
+      toFill.addCommand("brief", comment.text)
+    end
+  end
+
+  # Extract a block command comment - like /brief
+  def extractBlockCommandComment(toFill, comment)
+    toFill.addCommand(comment.name, comment.comment)
+  end
+
+  # Extract an inline command comment
+  def extractInlineCommandComment(toFill, comment)
+    toFill.addCommand(comment.name, "")
+  end
+
+  # Extract a param command comment like /param
+  def extractParamCommandComment(toFill, comment)
+    if(comment.valid_index?)
+      toFill.addParam(comment.index, comment.comment, comment.direction_explicit?, comment.direction)
+    end
   end
 end
