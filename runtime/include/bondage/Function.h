2e20c27b35227b63634d3beb9bc0fcae6fa488e2
Jorj <jorjpimm@gmail.com>
2014-03-27 08:37:18 +0000

    starting to add function calling.

--- runtime/include/bondage/Function.h
+++ runtime/include/bondage/Function.h
@@ -5,6 +5,65 @@ namespace bondage
 
 class Function
   {
+public:
+  class Arguments
+    {
+  public:
+    void **_args;
+    void *_this;
+    void *_result;
+    };
+  typedef Arguments *CallData;
+
+  typedef void (*Call)(Arguments *);
+
+  Function(Call fn)
+      : m_function(fn)
+    {
+    }
+
+  void call(Arguments *a)
+    {
+    m_function(a);
+    }
+
+private:
+  Call m_function;
+  };
+
+class FunctionCaller
+  {
+public:
+  typedef Function::Arguments *CallData;
+  typedef Function Result;
+
+  template <typename Builder> static Result build()
+    {
+    return Function(call<Builder>);
+    }
+
+  template <typename Builder> static void call(CallData data)
+    {
+    Builder::call(data);
+    }
+
+
+  template <typename T> static T getThis(CallData args)
+    {
+    return (T)args->_this;
+    }
+
+  template <std::size_t I, typename Arg>
+      static Arg unpackArgument(CallData args)
+    {
+    typedef typename std::remove_reference<Arg>::type NoRef;
+    return *(NoRef*)args->_args[I];
+    }
+
+  template <typename Return, typename T> static void packReturn(CallData data, T &&result)
+    {
+    *(Return*)data->_result = result;
+    }
   };
 
 }
--- runtime/include/bondage/FunctionBuilder.h
+++ runtime/include/bondage/FunctionBuilder.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "bondage/Function.h"
+#include "Reflect/FunctionBuilder.h"
+#include "Reflect/MethodInjectorBuilder.h"
 
 namespace bondage
 {
@@ -9,21 +11,28 @@ class FunctionBuilder
 public:
   template <typename Fn> static Function build(const char *)
     {
-    return Function();
+    typename Fn::binder bind;
+
+    return bind.template buildInvocation<typename Fn::Caller>();
     }
 
   template <typename... Overloads> static Function buildOverloaded(const char *)
     {
-    return Function();
+    return Function(nullptr);
     }
 
 
   template <typename Signature, Signature Fn> class buildCall
     {
+  public:
+    typedef Reflect::FunctionBuilder<Signature, Fn> binder;
+    typedef FunctionCaller Caller;
     };
 
   template <typename Signature, Signature Fn> class buildMemberStandinCall
     {
+    typedef Reflect::FunctionBuilder<Signature, Fn> binder;
+    typedef Reflect::MethodInjectorBuilder<FunctionCaller> Caller;
     };
   };
 
