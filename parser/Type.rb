e3a0d82a1703f51a6a5b9f06a9cb8ce56c3dad23
George Pimm <george.pimm@naturalmotion.com>
2014-05-13 14:01:56 +0100

    add functions to type for querying required types, and a bindable name (used in arg lists)

--- parser/Type.rb
+++ parser/Type.rb
@@ -108,6 +108,10 @@ class Type
       @canonical.kind == :type_longdouble
   end
 
+  def isPod
+    return isInteger() || isStringLiteral() || isFloatingPoint() || isBoolean() || isVoid()
+  end
+
   # find a pretty string to represent the type
   def prettyName
     return "#{@type.spelling}"
@@ -128,22 +132,16 @@ class Type
     return Type.stripTemplates(n)
   end
 
-  # find a name, without decoration or templating for the type, but includeing namespacesss
-  def nameWithTypedefs
-    n = @type.spelling
-    if(isConstQualified)
-      n.sub!("const ", "")
-    end
-
-    return Type.stripTemplates(n)
-  end
-
   # find a short name, without decoration or templating for the type.
   def shortName
     n = name
     idx = n.rindex("::")
 
     return n[(idx+2)..n.length]
+  end
+
+  def bindableName
+    return @type.spelling
   end
 
   def fullyQualifiedName
@@ -187,6 +185,19 @@ class Type
     end
 
     return Type.new(@type.result_type)
+  end
+
+  def getRequiredTypes(types)
+    if (isPointer || isLValueReference || isRValueReference)
+      pointeeType.getRequiredTypes(types)
+    elsif (!isPod())
+      types << fullyQualifiedName
+    end
+
+    templateArgCount.times do |i|
+      t = templateArg[i]
+      t.getRequiredTypes(types)
+    end
   end
 
   # Find the number of template arguments
